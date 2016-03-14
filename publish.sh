e46eec620579f9234b599e3302692c0bf390517e
yue.chen <yue.chen@yingzt.com>
2016-03-14 21:13:02 +0800

    publish.sh test

--- publish.sh
+++ publish.sh
@@ -12,10 +12,12 @@ if [ $# == 1 ]
 then
     git add .
     git ci -a -m "$1"
+    git push origin master
     ghp-import -m "$1" _book -p
 else
     git add .
     git ci -a -m "update data"
+    git push origin master
     ghp-import _book -p
 fi
 
