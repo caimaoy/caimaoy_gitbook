1ef8bd012ec5f6b8f894c0721dbbf259ec9fc244
yue.chen <yue.chen@yingzt.com>
2016-03-14 21:18:34 +0800

    test_publish

--- publish.sh
+++ publish.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-if git pull origin master
-then
-    echo "pull success"
-else
-    exit $?
-fi
-
 
 if [ $# == 1 ]
 then
     git add .
     git ci -a -m "$1"
+    git pull origin master
     git push origin master
     ghp-import -m "$1" _book -p
 else
     git add .
     git ci -a -m "update data"
+    git pull origin master
     git push origin master
     ghp-import _book -p
 fi
